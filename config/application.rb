3f58dc3285d9934986f8da6c55896aac822cbf60
owenhaupt <owenshaupt@gmail.com>
2019-09-06 22:44:34 -0700

    all files here pertaining to css are examples for loading custom fonts

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module Candidpop
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    # fonts???
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
