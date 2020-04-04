bfda705d1a30228ce414637e5097f5b830dfcf60
owenshaupt <owenshaupt@gmail.com>
2020-04-03 17:27:41 -0700

    updated actionview gem

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,9 +1,9 @@
-import * as APIUtil from '../util/session_api_util';
+import * as APIUtil from "../util/session_api_util";
 
-export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
-export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
-export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
-export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
 
 const receiveCurrentUser = currentUser => ({
   type: RECEIVE_CURRENT_USER,
@@ -23,13 +23,15 @@ export const clearErrors = () => ({
   type: CLEAR_ERRORS
 });
 
-export const signup = user => dispatch => APIUtil.signup(user)
-  .then(user => dispatch(receiveCurrentUser(user)))
-  .fail(error => dispatch(receiveErrors(error.responseJSON)));
+export const signup = user => dispatch =>
+  APIUtil.signup(user)
+    .then(user => dispatch(receiveCurrentUser(user)))
+    .fail(error => dispatch(receiveErrors(error.responseJSON)));
 
-export const login = user => dispatch => APIUtil.login(user)
-  .then(user => dispatch(receiveCurrentUser(user)))
-  .fail(error => dispatch(receiveErrors(error.responseJSON)));
+export const login = user => dispatch =>
+  APIUtil.login(user)
+    .then(user => dispatch(receiveCurrentUser(user)))
+    .fail(error => dispatch(receiveErrors(error.responseJSON)));
 
-export const logout = () => dispatch => APIUtil.logout()
-    .then(() => dispatch(logoutCurrentUser()));
+export const logout = () => dispatch =>
+  APIUtil.logout().then(() => dispatch(logoutCurrentUser()));
